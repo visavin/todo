d3952af701c28e37bf84bd0a9c12af06bf14e425
visavin <visavin@yandex.ru>
2022-11-17 17:07:25 +0300

    TODO App - Tooling и развертывание приложения (Refactoring)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,110 +1,113 @@
-import { Component } from 'react'
+import { Component } from "react";
+import { v4 as uuid } from "uuid";
 
-import './App.css'
-import NewTaskForm from '../NewTaskForm'
-import TaskList from '../TaskList'
-import Footer from '../Footer'
+import "./App.css";
+import NewTaskForm from "../NewTaskForm";
+import TaskList from "../TaskList";
+import Footer from "../Footer";
 
 export default class App extends Component {
-  startId = 0
-
   state = {
     todoData: [
-      this.createTodoItem('Task number 1'),
-      this.createTodoItem('Task number 2'),
-      this.createTodoItem('Task number 3'),
+      this.createTodoItem("Task number 1"),
+      this.createTodoItem("Task number 2"),
+      this.createTodoItem("Task number 3"),
     ],
-    statusFilter: 'All',
-  }
+    statusFilter: "All",
+  };
 
   createTodoItem(description) {
     return {
       description,
       completed: false,
       editing: false,
-      id: this.startId++,
+      id: uuid(),
       addDate: new Date(),
-    }
+    };
   }
 
   toggleProperty(arr, id, property) {
-    const index = arr.findIndex((element) => element.id === id)
-    const oldItem = arr[index]
-    const newItem = { ...oldItem, [property]: !oldItem[property] }
-    return [...arr.slice(0, index), newItem, ...arr.slice(index + 1)]
+    const index = arr.findIndex((element) => element.id === id);
+    const oldItem = arr[index];
+    const newItem = { ...oldItem, [property]: !oldItem[property] };
+    return [...arr.slice(0, index), newItem, ...arr.slice(index + 1)];
   }
 
   onToggleCompleted = (id) => {
     this.setState(({ todoData }) => ({
-      todoData: this.toggleProperty(todoData, id, 'completed'),
-    }))
-  }
+      todoData: this.toggleProperty(todoData, id, "completed"),
+    }));
+  };
 
   onToggleEditing = (id) => {
     this.setState(({ todoData }) => ({
-      todoData: this.toggleProperty(todoData, id, 'editing'),
-    }))
-  }
+      todoData: this.toggleProperty(todoData, id, "editing"),
+    }));
+  };
 
   editItem = (id, description) => {
     this.setState(({ todoData }) => {
-      const index = todoData.findIndex((element) => element.id === id)
-      const oldItem = todoData[index]
-      const newItem = { ...oldItem, description: description }
-      const newArray = [...todoData.slice(0, index), newItem, ...todoData.slice(index + 1)]
+      const index = todoData.findIndex((element) => element.id === id);
+      const oldItem = todoData[index];
+      const newItem = { ...oldItem, description: description };
+      const newArray = [
+        ...todoData.slice(0, index),
+        newItem,
+        ...todoData.slice(index + 1),
+      ];
       return {
         todoData: newArray,
-      }
-    })
-  }
+      };
+    });
+  };
 
   addItem = (description) => {
-    const newItem = this.createTodoItem(description)
+    const newItem = this.createTodoItem(description);
     this.setState(({ todoData }) => {
-      const newTodoData = [].concat(todoData)
-      newTodoData.push(newItem)
       return {
-        todoData: newTodoData,
-      }
-    })
-  }
+        todoData: [...todoData, newItem],
+      };
+    });
+  };
 
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
-      const newTodoData = todoData.filter((item) => item.id !== id)
+      const newTodoData = todoData.filter((item) => item.id !== id);
       return {
         todoData: newTodoData,
-      }
-    })
-  }
+      };
+    });
+  };
 
   deleteCompleted = () => {
     this.setState(({ todoData }) => {
-      const newTodoData = todoData.filter((item) => !item.completed)
+      const newTodoData = todoData.filter((item) => !item.completed);
       return {
         todoData: newTodoData,
-      }
-    })
-  }
+      };
+    });
+  };
 
   onToggleStatus = (status) => {
     this.setState(() => ({
       statusFilter: status,
-    }))
-  }
+    }));
+  };
 
   todoFiltered = () => {
-    if (this.state.statusFilter === 'Completed') {
-      return this.state.todoData.filter((item) => item.completed)
-    } else if (this.state.statusFilter === 'Active') {
-      return this.state.todoData.filter((item) => !item.completed)
+    if (this.state.statusFilter === "Completed") {
+      return this.state.todoData.filter((item) => item.completed);
+    } else if (this.state.statusFilter === "Active") {
+      return this.state.todoData.filter((item) => !item.completed);
     } else {
-      return this.state.todoData
+      return this.state.todoData;
     }
-  }
+  };
 
   render() {
-    const todoCount = this.state.todoData.filter((element) => !element.completed).length
+    const todoCount = this.state.todoData.filter(
+      (element) => !element.completed
+    ).length;
 
     return (
       <section className="todoapp">
@@ -128,6 +131,6 @@ export default class App extends Component {
           />
         </section>
       </section>
-    )
+    );
   }
 }
--- src/components/TasksFilter/TasksFilter.js
+++ src/components/TasksFilter/TasksFilter.js
@@ -1,41 +1,40 @@
-import { Component } from 'react'
-import './TasksFilter.css'
-import PropTypes from 'prop-types'
+import { Component } from "react";
+import "./TasksFilter.css";
+import PropTypes from "prop-types";
 
 export default class TasksFilter extends Component {
   static defaultProps = {
-    statusFilter: 'All',
+    statusFilter: "All",
     onToggleStatus: () => {},
-  }
+  };
 
   static propTypes = {
     statusFilter: PropTypes.string.isRequired,
     onToggleStatus: PropTypes.func,
-  }
+  };
+
+  state = {
+    filters: ["All", "Active", "Completed"],
+  };
 
   setFilter = (event) => {
-    this.props.onToggleStatus(event.target.innerText)
-  }
+    this.props.onToggleStatus(event.target.innerText);
+  };
 
   render() {
-    return (
-      <ul className="filters">
-        <li>
-          <button className={this.props.statusFilter === 'All' ? 'selected' : null} onClick={this.setFilter}>
-            All
+    const buttonItems = this.state.filters.map((item, index) => {
+      return (
+        <li key={index}>
+          <button
+            className={this.props.statusFilter === item ? "selected" : null}
+            onClick={this.setFilter}
+          >
+            {item}
           </button>
         </li>
-        <li>
-          <button className={this.props.statusFilter === 'Active' ? 'selected' : null} onClick={this.setFilter}>
-            Active
-          </button>
-        </li>
-        <li>
-          <button className={this.props.statusFilter === 'Completed' ? 'selected' : null} onClick={this.setFilter}>
-            Completed
-          </button>
-        </li>
-      </ul>
-    )
+      );
+    });
+
+    return <ul className="filters">{buttonItems}</ul>;
   }
 }
